7e874d725a2862c7f4656425333a95b09cedc0ab
Sirius0v0 <1694605269@qq.com>
2023-07-26 14:29:53 +0800

    :tada: Added exceptions

--- KFExceptions/include/KFExceptions/KFExceptions.hpp
+++ KFExceptions/include/KFExceptions/KFExceptions.hpp
@@ -8,30 +8,30 @@ namespace kalmans
     struct KFException : public std::logic_error
     {
         explicit KFException(const std::string& message)
-            :logic_error(message){}
+            :logic_error(message) {}
     };
 
     struct LengthError : public KFException
     {
         explicit LengthError(const std::string& message)
-            :KFException("[LengthError] "+message) {}
+            :KFException("[LengthError] " + message) {}
     };
 
     struct OutOfRangeError : public KFException
     {
         explicit OutOfRangeError(const std::string& message)
-            :KFException("[OutOfRangeError] "+message){}
+            :KFException("[OutOfRangeError] " + message) {}
     };
 
     struct ValueError : public KFException
     {
         explicit ValueError(const std::string& message)
-            :KFException("[ValueError] "+message){}
+            :KFException("[ValueError] " + message) {}
     };
 
     struct ZeroDivisionError : public KFException
     {
         explicit ZeroDivisionError(const std::string& message)
-            :KFException("[ZeroDivisionError] "+message){}
+            :KFException("[ZeroDivisionError] " + message) {}
     };
 }

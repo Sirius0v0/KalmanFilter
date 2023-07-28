a9f8f187082bcce40345b6594dab8ad6f46885b5
Sirius0v0 <1694605269@qq.com>
2023-07-28 16:39:00 +0800

    :tada: Added Filter class

---
+++ example/filter_demo/src/filter_test.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
+#include <Filter/Filter.hpp>
+
+int main()
+{
+    std::cout << "Hello My Filter~\n";
+
+    return EXIT_SUCCESS;
+}
